4136a1ed1b85bea77a0b943ad1b0851843f1d713
Mike deCock <evilgnome6@gmail.com>
2014-03-23 01:24:57 -0700

    Added 054

--- 051 - 100/053 - Combinatoric selections/053 - Combinatoric selections.py
+++ 051 - 100/053 - Combinatoric selections/053 - Combinatoric selections.py
@@ -21,5 +21,7 @@ def getcomb(n, r):
 count = 0
 for n in range(3, 101):
 	for r in range(1, n):
-		if getcomb(n, r) > 1000000: count += 1
+		if getcomb(n, r) > 1000000: 
+			count += (n-(2*r-1))
+			break
 print count
