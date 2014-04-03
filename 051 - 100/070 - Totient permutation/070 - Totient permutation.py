7fbd0e41f06f5cf3365de4a496373698b5547b79
Mike deCock <evilgnome6@gmail.com>
2014-04-03 03:20:10 -0700

    Finished 070

--- 051 - 100/070 - Totient permutation/070 - Totient permutation.py
+++ 051 - 100/070 - Totient permutation/070 - Totient permutation.py
@@ -6,33 +6,39 @@
 
 #Find the value of n, 1 < n < 107, for which φ(n) is a permutation of n and the ratio n/φ(n) produces a minimum.
 
-limit = 10000
+#in order to minimize n/φ(n), we are looking for a product of two prime factors close to 10^7
+#the √ of 10^7 = 3162.28 so we'll generate primes between 2000 and 4000 since one of the primes will be below that number and the other will be above it
+
+limit = 4000
 primes = []
 sieve = [True]*limit
 
 for i in range(2, limit):
 	if sieve[i] == True:
-		primes.append(i)
+		if i > 2000: primes.append(i)
 		for j in range(i*i, limit, i):
 			sieve[j] = False
 
 primeset = set(primes)
-#print primes
 
-def getfactors(number):
-	factors = []
-	if number in primeset:
-		factors.append(number)
-		return factors
-	for p in primes:
-		if number % p == 0:
-			factors.append(p)
-			while number % p == 0:
-				number /= p	
-			if number in primeset:
-				factors.append(number)
-				return factors
-	return factors
+#For a semiprime n = pq the value of Euler's totient function (the number of positive integers less than or equal to n that are relatively prime to n) is particularly simple when p and q are distinct:
+#φ(n) = (p − 1) (q − 1)
 
-for i in range(2, 10000):
-	print i, getfactors(i)
+def gettotient(p, q):
+	return (p - 1) * (q - 1)
+	
+minratio = 2.0
+
+for i in range(0, len(primes)):
+	factor1 = primes[i]
+	for j in range(i, len(primes)):
+		factor2 = primes[j]
+		number = factor1 * factor2
+		if number > 10000000: break
+		totient = gettotient(factor1, factor2)
+		if sorted(str(number)) == sorted(str(totient)):
+			ratio = float(number) / totient
+			if ratio < minratio:
+				minratio = ratio
+				print number, factor1, factor2, totient, ratio
+
