0c9f906ab56409741d13dc5d3f36dc604c40f18f
Mike deCock <evilgnome6@gmail.com>
2014-04-21 22:25:49 -0700

    Completed 082

--- 051 - 100/082 - Path sum- three ways/082 - Path sum- three ways.py
+++ 051 - 100/082 - Path sum- three ways/082 - Path sum- three ways.py
@@ -10,7 +10,7 @@
 	
 #Find the minimal path sum, in matrix.txt (right click and 'Save Link/Target As...'), a 31K text file containing a 80 by 80 matrix, from the left column to the right column.
 
-rows = 5 #80
+rows = 80
 matrix = []
 file = open("matrix.txt")
 
@@ -21,15 +21,48 @@ for i in range(rows):
 
 file.close()
 
-matrix = [[131,673,234,103,18],[201,96,342,965,150],[630,803,746,422,111],[537,699,497,121,956],[805,732,524,37,331]]
+#matrix = [[131,673,234,103,18],[201,96,342,965,150],[630,803,746,422,111],[537,699,497,121,956],[805,732,524,37,331]]
 
-for row in matrix: print row
+visited = dict()
+completed = dict()
 
-visited = [[0]*rows]*rows
-completed = [[0]*rows]*rows
+#start at the left column add all the nodes to visited
 
-for row in visited: print row
+for i in range(rows):
+	node = (i,0)
+	visited[node] = matrix[i][0]
+	
+while True:
 
-#start at the left column to find the smallest cost entry point
+	#find the lowest value node, add it to completed and remove it from visited
 
-print min(matrix)
+	node = min(visited.items(), key=lambda x: x[1])
+	completed[node[0]] = node[1]
+	del visited[node[0]]
+
+	#find neighboring nodes (up/down/right).
+
+	neighbors = dict()
+
+	if node[0][0] > 0: neighbors[(node[0][0]-1,node[0][1])] = 0
+	if node[0][0] < rows-1: neighbors[(node[0][0]+1,node[0][1])] = 0
+	if node[0][1] < rows-1: neighbors[(node[0][0],node[0][1]+1)] = 0
+
+	#sum path to neighbors and add sum to visited if sum is lower.
+
+	for nnode in neighbors:
+		if nnode not in completed:
+			if nnode not in visited:
+				visited[nnode] = node[1] + matrix[nnode[0]][nnode[1]]
+			else:
+				if visited[nnode] < node[1] + matrix[nnode[0]][nnode[1]]:
+					visited[nnode] < node[1] + matrix[nnode[0]][nnode[1]]
+	if len(visited) == 0: break
+
+#collect the values on the right column from completed and print the minimum.
+
+rcolumn = []
+for node in completed:
+	if node[1] == rows-1: rcolumn.append(completed[node])
+print min(rcolumn)
+
