cfd31016c8cfd12e0bda9dffa3b0984b3f0404bc
Mike deCock <evilgnome6@gmail.com>
2014-05-04 05:02:55 -0700

    Updated 001

--- 001 - 050/001 - Multiples of 3 and 5/001 - Multiples of 3 and 5.py
+++ 001 - 050/001 - Multiples of 3 and 5/001 - Multiples of 3 and 5.py
@@ -1,11 +1,9 @@
 #Find the sum of all the multiples of 3 or 5 below 1000.
 
-i = 1
 sum = 0
 
-while i in range(1, 1000):
+for i in range(1, 1000):
 	if (i % 3 == 0) or (i % 5 == 0):
-		sum = sum + i
-	i = i + 1
+		sum += i
 
 print(sum)
