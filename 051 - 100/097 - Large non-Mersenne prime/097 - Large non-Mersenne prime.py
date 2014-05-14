1e1e536c80905b8d79924cf631679f763e5c2a15
Mike deCock <evilgnome6@gmail.com>
2014-05-13 22:08:16 -0700

    Added 097

---
+++ 051 - 100/097 - Large non-Mersenne prime/097 - Large non-Mersenne prime.py
@@ -0,0 +1,22 @@
+#-*- encoding:utf-8 -*-
+#The first known prime found to exceed one million digits was discovered in 1999, and is a Mersenne prime of the form 2^6972593−1; it contains exactly 2,098,960 digits. Subsequently other Mersenne primes, of the form 2^p−1, have been found which contain more digits.
+
+#However, in 2004 there was found a massive non-Mersenne prime which contains 2,357,207 digits: 28433×2^7830457+1.
+
+#Find the last ten digits of this prime number.
+
+from time import time
+
+t = time()
+
+number = 2
+
+for i in range(1, 7830457):
+	number *= 2
+	number %= 10**10
+
+number *= 28433
+number %= 10**10
+number += 1
+
+print number, time()-t
