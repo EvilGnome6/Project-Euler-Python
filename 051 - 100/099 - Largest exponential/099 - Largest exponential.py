d2ab50cb42eac57f0ac70fa46342b14e4677b9aa
Mike deCock <evilgnome6@gmail.com>
2014-05-18 21:01:42 -0700

    Finished 099

--- 051 - 100/099 - Largest exponential/099 - Largest exponential.py
+++ 051 - 100/099 - Largest exponential/099 - Largest exponential.py
@@ -6,12 +6,19 @@
 
 #NOTE: The first two lines in the file represent the numbers in the example given above.
 
+# copy all the base/exponent pairs in the base_exp.txt file to a basexp list
+basexp = []
+file = open("base_exp.txt")
 
+for i in range(1000):
+	line = file.readline()
+	line = line.strip()
+	pair = line.split(',')
+	basexp.append([int(pair[0]),int(pair[1]), i+1])
+
+# run through the list of pairs and track which is the largest.
 from math import log10
-
-if 2**11 < 3**7: print "N"
-if log10(2)*11 < log10(3)*7: print "N"
-
-if 632382**518061 > 519432**525806: print "Y"
-if log10(632382)*518061 > log10(519432)*525806: print "Y"
-
+maxpair = [1,1,0]
+for pair in basexp:
+	if log10(pair[0])*pair[1] > log10(maxpair[0])*maxpair[1]: maxpair = pair
+print maxpair
